cbb6975b4acb63f1dc420f32266df7af023076fc
Kyle <none>
2017-11-13 20:49:52 -0800

    test

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ from app import create_app, init_db
 app = create_app("default")
 
 if __name__ == "__main__":
+    print("INITIALIZE DATABASE AND START APP")
     init_db()
     app.run()
-    
+else:
+    print("INIITALIZE DATABASE")
+    init_db()
