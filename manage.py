5d9dfecb2963accb1b0b9edde296e15af15ade58
Kyle <none>
2017-11-14 22:23:31 -0800

    update

--- manage.py
+++ manage.py
@@ -3,14 +3,14 @@ from app import create_app, init_db
 
 app = create_app("default")
 
+from flask_script import Manager, Shell
 
-@app.before_first_request
-def before_first_request():
+manager = Manager(app)
+
+@manager.command
+def init():
     init_db()
 
 if __name__ == "__main__":
-    print("INITIALIZE DATABASE AND START APP")
     # init_db()
-    app.run()
-else:
-    print("STARTING")
+    manager.run()
